4791926a53d0e06dd8141b64c2c5ceed58819f28
Igor <garrikwower@gmail.com>
2020-11-15 19:11:14 +0300

    api и сайт объединены

---
+++ web/app.py
@@ -0,0 +1,64 @@
+import flask
+from flask import jsonify, request, render_template
+from core.provider import Provider
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        user_id = int(request.form['user_id'])
+        recom = DATA_PROVIDER.get_user_recomendations(user_id)
+        hist = DATA_PROVIDER.get_user_history(user_id)
+
+        data = {
+            'history': hist,
+            'recommendations': recom
+        }
+    else:
+        data = None
+    return render_template('main.html', data=data)
+
+
+@app.route('/about', methods=['GET'])
+def about():
+    return render_template('team.html')
+
+
+# region API
+@app.route('/api/v1/recommendations/get', methods=['GET'])
+def api_get_recommendations():
+    # парсим аргументы запроса
+    args = request.args
+    user_id = int(args['user_id'])
+
+    # Собираем ответ
+    user_info = {'user_id': user_id}
+    recommendations = DATA_PROVIDER.get_user_recomendations(user_id)
+    response = {"user": user_info, "recommendations": recommendations}
+
+    return make_api_response(response, 200)
+
+
+@app.route('/api/v1/restaurant/<restaurant_id>', methods=['GET'])
+def api_get_restaurant_info(restaurant_id):
+    print(restaurant_id)
+    resp = DATA_PROVIDER.get_restaraunt_info(restaurant_id)
+    return make_api_response(resp, 200)
+
+
+def make_api_response(response_object, code):
+    return jsonify(response_object), code
+
+
+# endregion
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<h1>404</h1><p>This API method does not exist.</p>", 404
+
+
+DATA_PROVIDER = Provider()
+app.run()
--- web/core/provider.py
+++ web/core/provider.py
@@ -25,6 +25,29 @@ class Provider():
 
         return tmp_recomendations.get(user_id, [])
 
+    def get_user_history(self, user_id):
+        """Возвращает рекомендации пользователя"""
+        tmp_recomendations = {
+            1: [{"id": 3,
+                 "image": "/images/img3.png",
+                 "name": 'Restaurant 3',
+                 "rank": 2},
+                {"id": 4,
+                 "image": "/images/img4.png",
+                 "name": 'Restaurant 4',
+                 "rank": 1}],
+            2: [{"id": 1,
+                 "image": "/images/img1.png",
+                 "name": 'Restaurant 1',
+                 "rank": 1},
+                {"id": 2,
+                 "image": "/images/img2.png",
+                 "name": 'Restaurant 2',
+                 "rank": 2}]
+        }
+
+        return tmp_recomendations.get(user_id, [])
+
     def get_restaraunt_info(self, restaurant_id):
         """Возвращает информацию по выбранному ресторану"""
         tmp_rest_info = {
